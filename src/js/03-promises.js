7ec96dd3ee81b289d34db0f545a54ea888ffd967
Tata-Link <83451272+Tata-git@users.noreply.github.com>
2022-06-01 16:42:08 +0300

    Update 03-promises.js
    error Expected a JavaScript module script but the server responded with a MIME type of "text/html".

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,26 +1,56 @@
 import Notiflix from 'notiflix';
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const refs = {
   form: document.querySelector('.form'),
 };
 // console.dir(refs.form);
 
 // при сабмите формы вызывает функцию createPromise(position, delay) столько раз, сколько ввели в поле amount
-refs.form.addEventListener('submit', () => {});
+refs.form.addEventListener('submit', submitForm);
+
+function submitForm(evt) {
+  evt.preventDefault();
+
+  let firstDelayValue = +refs.form.delay.value;
+  const delayStepValue = +refs.form.step.value;
+  const amountValue = +refs.form.amount.value;
+
+  for (let position = 0; position <= amountValue; position += 1) {
+    firstDelayValue += delayStepValue;
+
+    createPromise(position, firstDelayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+
+  evt.currentTarget.reset();
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+//----------------------------------------------------------------
+// const firstDelayValue2 = refs.form.delay.value;
+// console.log(typeof firstDelayValue2); // string
+//------ error---------------------
+// const firstDelayValue = +refs.form.delay.value;
+// firstDelayValue += delayStepValue; // Error: Assignment to constant variable.  at HTMLFormElement.submitForm
+//----->>>>> let firstDelayValue = +refs.form.delay.value;
